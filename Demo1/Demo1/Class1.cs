e3438e23c2428cc0606ca92fd180ccc7b523c4f8
JitendraBordiya <Jitendra7496@gmail.com>
2021-08-29 10:23:05 +0530

    Program For Fibonacci Series.StarPattern,

---
+++ Demo1/Demo1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            int a = 66;
+            int b = 27;
+            int c = a + b;
+            Console.WriteLine("The Value of First Variable is{0}", a);
+            Console.WriteLine("The Value of First Variable is " + b);
+            Console.WriteLine("The Addition of{0} and {1} = {2}", a, b, c);
+            Console.WriteLine("{0} is the total of {1} and {2} ", c, a, b);
+            Console.Read();
+        }
+    }
+}
---
+++ Demo1/Demo1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DotNet
+{
+    class Program
+    {
+      static void Main(String[] args)
+        {
+           //fibonacci Series
+            int num1 = 0;
+            int num2 = 1;
+            int n;
+            Console.Write("Enter The Value of n :");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write(num1+" "+num2);
+
+             for(int i = 0; i <= n; i++)
+            {
+              int num3 = num1 + num2;
+            Console.Write(" " + num3);
+            num1 = num2;
+             num2 = num3;
+            }
+
+             //Pattern Program
+             Console.WriteLine();
+            for(int i = 0; i <= n; i++)
+            {
+            for(int j = 0; j <= i; j++)
+            {
+
+               Console.Write("*");
+            }
+            Console.WriteLine();
+            }
+            int n1, n2;
+            n1 = n2 = 24;
+            Console.WriteLine("The Value of First Variable is {0}", n1);
+            Console.WriteLine("The Value of First Variable is "+ n2);
+            int x = 10;
+            bool result = (!(x == 10));
+            Console.WriteLine("The Condition is " + result);
+          
+            
+
+
+            Console.Read(); }
+    }
+}
