42c60a6f8dd712b50e9aed605e30242b55e4ea0e
JitendraBordiya <Jitendra7496@gmail.com>
2021-09-03 14:02:40 +0530

    Program to Sum of Two Number if Value of Two Number Is Zero of Greater Than 10000  its Caught Error if Condition is False It Performd  Sum Operation

---
+++ DemoUserDefinedException/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace DemoUserDefinedException
+{
+    class Addition
+    {
+
+        public void Sum(int a, int b)
+        {
+            if (b == 0 || b >= 10000 && a == 0 || a >= 10000)
+            {
+                throw (new IntegerOutOfRnge("Values Should be Greater Than Zero Or Less Than TEN Thousand !!"));
+            }
+            else
+            {
+                int c = a + b;
+                Console.WriteLine("The Sum is " + c);
+            }
+        }
+    }
+
+        public class IntegerOutOfRnge : ApplicationException
+    {
+       
+             
+        public  IntegerOutOfRnge(string message):base(message)
+            {
+              
+            }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Addition ad = new Addition();
+           
+            try
+            {
+                Console.WriteLine("Enter Two Numbers ");
+                Console.Write("A := ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("B := ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                ad.Sum(a, b);
+
+            }
+            catch(IntegerOutOfRnge e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
