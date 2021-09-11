864f07f8d293a7cfc26867366c21c8d836eceb93
JitendraBordiya <Jitendra7496@gmail.com>
2021-09-11 18:22:18 +0530

    Program LAmbda Expression

---
+++ LambdaExpression/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using System.Collections.Generic;
+namespace LambdaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            List<int> evenNumbers = list.FindAll(x => (x % 2) == 0);
+            List<int> oddNumber = list.FindAll(x => (x % 2) != 0);
+            Console.Write("List Of EvenNumbers : ");
+          foreach (var num in evenNumbers)
+                             {
+                  Console.Write("{0} ", num);
+                            }
+            Console.WriteLine();
+            Console.Write("List Of oddNumber : ");
+            foreach (var obj in oddNumber)
+            {
+                Console.Write("{0} ", obj);
+            }
+            Console.WriteLine();
+                Console.Read();
+         }
+    }
+}
