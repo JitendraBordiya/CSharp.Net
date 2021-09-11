4e2a6d2405683c222d33fac0201f962f192b787c
JitendraBordiya <Jitendra7496@gmail.com>
2021-09-11 18:26:08 +0530

    Program on Exetension Method

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+    class Flower
+    {
+        public  void Lily(){
+            Console.Write(" National Flower is Lily");
+            
+           }
+        public  void Jasmine()
+        {
+            Console.Write("National Flower is Jasmine");
+             }
+    }
+   static class  Country
+    {
+        public static void England(this Flower F)
+        {
+            Console.Write("This is England and ");
+        }
+        public static void France(this Flower F)
+        {
+            Console.Write("This is France and ");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flower f1 = new Flower();
+           
+           
+            f1.England();
+            f1.Lily();
+            Console.WriteLine();
+            f1.France();
+            f1.Jasmine();
+            Console.Read();
+
+
+        }
+    }
+}
