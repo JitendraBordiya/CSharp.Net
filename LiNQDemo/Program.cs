1488a14fd38669eef01853786f93447acd6a0359
JitendraBordiya <Jitendra7496@gmail.com>
2021-09-13 18:05:40 +0530

    LinQ to SQL

---
+++ LiNQDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+namespace LiNQDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = {"Jitendra","Rishabh"," Rajan"};
+
+            var query = from name in names
+                        select name;
+         foreach(var n in query)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
---
+++ LiNQDemo2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiNQDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conn = "Data Source=LAPTOP-FS8H47HQ\\DBSERVER;Initial Catalog=MyTQdb;Integrated Security=true";
+            DataClasses1DataContext emp = new DataClasses1DataContext(conn);
+            Employee e1 = new Employee();
+            e1.Name = "Rajan";
+            e1.Email = "Rajan18@mail.com";
+            e1.ContactNo = "7488938777";
+            e1.DeptId = 2;
+            e1.Address = "gwalior";
+            emp.Employees.InsertOnSubmit(e1);
+            emp.SubmitChanges();
+            Console.WriteLine("Record Inserted");
+            Console.ReadKey();
+
+
+        }
+    }
+}
