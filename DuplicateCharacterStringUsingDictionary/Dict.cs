52208c090ff8210a507c9b0fb09089f1de39c93b
Jitendra Bordiya <Jitendra7496@gmail.com>
2021-10-18 13:02:05 +0530

    Find Duplicates and Their Count

---
+++ DuplicateCharacterStringUsingDictionary/Dict.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace DuplicateCharacterStringUsingDictionary
+{
+    public class Find{
+        public static void FindDuplicate(string str)
+        {
+            string v = str.ToLower();
+            var arr = v.ToCharArray();
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            foreach (char ch in arr)
+            {
+                if (map.ContainsKey(ch))
+                {
+                    map[ch] = map[ch] + 1;
+                }
+                else
+                {
+                    map.Add(ch, 1);
+                }
+            }
+            var keys = new HashSet<char>(map.Keys);
+            foreach (char ch in keys)
+            {
+                if (map[ch] > 1)
+                {
+                    Console.WriteLine($"Character {ch} repeating {map[ch]} times.");
+                }
+            }
+
+
+        }
+        public Find()
+        {
+            Console.WriteLine("I am Constructor");
+        }
+        public void Grant()
+        {
+            var queue = new Queue<string>();
+            queue.Enqueue("Apple");
+            queue.Enqueue("Mango");
+            queue.Enqueue("Banana");
+            queue.Enqueue("Papaya");
+            foreach(string s in queue)
+            {
+                Console.WriteLine($"Fruit : {s}");
+            }
+
+        }
+    }
+    class Dict
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "JAVA OOPS COLLECTION ABSTRACTION INHERITANCE";
+            Find.FindDuplicate(s1);
+            Find f1 = new Find();
+            f1.Grant();
+
+        }
+    }
+}
