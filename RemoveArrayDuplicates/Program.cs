dd7d09097243a50d920ccb7dea6afc9ae9519ccd
Jitendra Bordiya <Jitendra7496@gmail.com>
2021-10-18 13:35:13 +0530

    Remove Duplicate Values From Array

---
+++ RemoveArrayDuplicates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RemoveArrayDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {4,5,7,6};
+            /*int[] dist = arr.Distinct().ToArray();
+            Console.WriteLine(dist.Length);
+            Console.WriteLine(arr.Length);
+            */
+            var dist = new HashSet<int>(arr);
+         
+            foreach (int i in dist)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
