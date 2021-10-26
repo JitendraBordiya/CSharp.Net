3db3c91dcf9aa095a4104ed873cba4b11ebc3a09
Jitendra Bordiya <Jitendra7496@gmail.com>
2021-10-26 16:51:26 +0530

    Program to Find Missing Number in Array Series

---
+++ FindMissingNumInArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FindMissingNumInArray
+{
+    class Program
+    {
+        public static int FindMissingNum(int[] array)
+        {
+            int n = array.Length + 1;
+            int sumofFirstNnum = n * (n + 1) / 2;
+            int actualSum = 0;
+            for(int i=0;i<array.Length;i++)
+            {
+                actualSum += array[i];
+            }
+            return sumofFirstNnum - actualSum;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 6, 7, 8 };
+            int missingNum = FindMissingNum(array);
+            Console.WriteLine($"The Mising Number is {missingNum}");
+
+        }
+    }
+}
