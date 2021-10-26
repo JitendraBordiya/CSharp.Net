044a360f23e015bd9232f0d000ff9ae8ff73b974
Jitendra Bordiya <Jitendra7496@gmail.com>
2021-10-26 11:28:29 +0530

    String Permutation

---
+++ StringPermutation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StringPermutation
+{
+    class Program
+    {
+        public static void PowerSet(string s, int i, string cur)
+        {
+            try
+            {
+                int n = s.Length;
+                if (i == n)
+                {
+                    Console.WriteLine(cur);
+                    return;
+                }
+                PowerSet(s, i + 1, cur + s[i]);
+                PowerSet(s, i + 1, cur);
+
+
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+        }
+        static void Main()
+        {
+            string str = "abc";
+            int index = 0;
+            string curr = "";
+            Program.PowerSet(str, index, curr);
+        }
+    }
+}
